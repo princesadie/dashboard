e661cf72d042c6df043fcc3e8345fff04d94261c
Prince Sadie <princesadie@gmail.com>
2016-09-11 11:17:55 -0500

    Add main image to properties

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, styles: { medium: "240x240>", thumb: "120x120>" }, default_url: "/images/:style/missing.png"
   has_many :properties
+  has_attached_file :avatar, styles: { medium: "240x240>", thumb: "120x120>" }, default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
