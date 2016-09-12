aeed78a86d5c7f0e00aa125f214c91590721dd77
Prince Sadie <princesadie@gmail.com>
2016-09-12 08:01:22 -0500

    Minor style updates

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,6 @@
 class Property < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :main_image, styles: { large: "1000x1000", medium: "240x240>", thumb: "120x120>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :main_image, styles: { large: "1000x1000>", medium: "240x240>", thumb: "120x120>" }, default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :main_image, content_type: /\Aimage\/.*\z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :properties
-  has_attached_file :avatar, styles: { medium: "240x240>", thumb: "120x120>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "120x120>" }, default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
